40c8553bc469d3a7585b8c78ac96e7f21b06695e
Emilia Ziemichód <emifrompoland@gmail.com>
2019-04-23 00:39:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,8 +6,12 @@ class EmailParser
   end
 
   def parse
-    @emails=@emails.gsub(/[\s,]/ ," ").split
-    @emails.uniq
+    #@emails=@emails.gsub(/[\s,]/ ," ").split
+    #@emails.uniq
+    #or
+    array=@emails.split(/[, ]/).uniq
+    array.reject! {|i| i.empty?}
+    array
   end
 
 end
